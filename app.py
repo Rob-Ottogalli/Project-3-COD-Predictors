98a14796689139c4f71dc1009d8576cf23b5998f
Chris Scranton <chscranton@gmail.com>
2020-07-17 17:58:49 -0500

    Added flask endpoints

--- app.py
+++ app.py
@@ -15,5 +15,30 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+    @app.route("/predict")
+def ml_app():
+    return render_template("ml.html")
+
+@app.route("/respitory_diseases")
+def respitory():
+    return render_template("index.html")
+
+@app.route("/external_causes")
+def external():
+    return render_template("index.html")
+
+@app.route("/nervous_system")
+def nervous():
+    return render_template("index.html")
+
+@app.route("/metabolic_disorders")
+def endocrine():
+    return render_template("index.html")
+
+@app.route("/mental_disorders")
+def mental():
+    return render_template("index.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
